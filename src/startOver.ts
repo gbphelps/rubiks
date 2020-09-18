74a2a9ad43486e60509f19019dc6d1e50f6999ff
Grant Phelps <gbphelps2@gmail.com>
2020-09-18 14:45:51 -0400

    Correctly cancel autocorrect twist action

--- src/startOver.ts
+++ src/startOver.ts
@@ -3,10 +3,20 @@ import { clear as clearHistory } from './history';
 import { shuffle } from './actions/updateRegistry';
 import { setRotation } from './rotation';
 import { reset as resetClock } from './clock';
-import { setAction } from './action';
+import { setAction, getAction } from './action';
 import { setUserEventsEnabled } from './events';
 
 export function startOver() {
+  const action = getAction();
+
+  if (action?.type === 'twist-autocorrect') {
+    action.params.tranche.forEach((box) => {
+      if (!box) return;
+      box.setRotationFromMatrix(new THREE.Matrix4().identity());
+      box.updateMatrix();
+    });
+  }
+
   setUserEventsEnabled(true);
   setAction(null);
   resetClock();
